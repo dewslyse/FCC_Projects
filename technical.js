6ef9de1136dd2504d8f35379434e458932ce5591
dewslyse <dewslyse@gmail.com>
2021-08-27 22:16:27 +0000

    Add technical documentation page to FCC solutions

--- technical.js
+++
@@ -1,6 +0,0 @@
-const hamburgerButton = document.getElementsByClassName('hamburger-button')[0];
-const navLinkSection = document.getElementsByClassName('nav-link-section')[0];
-
-hamburgerButton.addEventListener('click', () => {
-    navLinkSection.classList.toggle('active');
-});
